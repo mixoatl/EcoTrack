351d3f5010cf158d09bcfc412f408c51cf8d3809
Monserrat Fernández <xiomistar2003@hotmail.com>
2024-05-21 18:46:44 -0600

    Initial Commit

---
+++ EcoTrack/EcoTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoTrackApp.swift
+//  EcoTrack
+//
+//  Created by Monserrat Fernández on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct EcoTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
